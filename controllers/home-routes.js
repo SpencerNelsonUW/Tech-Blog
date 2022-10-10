81308bc153a739f7a9e4012822c76917644fe572
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-10-10 12:31:36 -0700

    beginning to implement the signup function but not currently operational

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+
+//Route for my home page, this is where posts will be shown
 router.get('/', (req, res) => {
     try {
         res.render('home')
@@ -7,5 +9,36 @@ router.get('/', (req, res) => {
         console.log(err);
     }
 });
+
+//route for the dashboard, AKA where new posts are made
+router.get('/dashboard', (req, res) => {
+    try {
+        res.render('dashboard')
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+//login to be able to view the posts and make new posts
+router.get('/login', (req, res) => {
+    try {
+        res.render('login')
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+//signup page for new users
+router.get('/signup', (req, res) => {
+    try {
+        res.render('signup')
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+
+
 
 module.exports = router;
